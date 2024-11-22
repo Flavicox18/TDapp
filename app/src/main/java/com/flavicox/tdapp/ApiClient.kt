ceb312d33cb706a54b0b2015debf8033c537cd34
jmlc643 <josemarialuyocampos@gmail.com>
2024-11-22 17:38:39 -0500

    Despliegue V1.5

--- app/src/main/java/com/flavicox/tdapp/ApiClient.kt
+++ app/src/main/java/com/flavicox/tdapp/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://turismodias-api.onrender.com/"
 
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
